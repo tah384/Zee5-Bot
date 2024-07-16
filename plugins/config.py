57eaa94626d3c41676c3b7ccb8eb13eeb6108439
tah384 <115026575+tah384@users.noreply.github.com>
2024-07-16 11:10:02 +0500

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -1,6 +1,6 @@
 import os
 
-class Config(object):
+class Config:
     # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7355006985:AAEY8ijg4CP-8GgcliRGJja87Tby78QT7To")
     # The Telegram API things
